d6ff11347f275558e1809e8337cdf2652baa2dda
Kashyap Kapdi <kapdikashyap2003@gmail.com>
2022-06-24 11:53:39 +0530

    All redux stuff hase completed for movie listing component

--- src/components/MovieListing/MovieListing.js
+++ src/components/MovieListing/MovieListing.js
@@ -1,9 +1,35 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { getAllMovies } from "../../features/Movies/MoviesSlice";
 
 import "./MovieListing.scss";
+import MoviesCard from "../MoviesCard/MoviesCard";
 
 const MovieListing = () => {
-  return <div>MovieListing</div>;
+  const movies = useSelector(getAllMovies);
+
+  let renderMovies = "";
+  renderMovies =
+    movies.Response === "true" ? (
+      movies.Search.map((movie, index) => {
+        <MoviesCard key={index} data={movie} />;
+      })
+    ) : (
+      <>
+        <div className="movies-error">
+          <h2>{movies.error}</h2>
+        </div>
+      </>
+    );
+
+  return (
+    <div className="movie-wrapper">
+      <div className="movie-list">
+        <h2>Movies</h2>
+        <div className="movie-container">{renderMovies}</div>
+      </div>
+    </div>
+  );
 };
 
 export default MovieListing;
