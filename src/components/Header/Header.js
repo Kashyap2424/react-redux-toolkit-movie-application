ef57648e688eb596c2320ea1d8dd2956e089157f
Kashyap Kapdi <kapdikashyap2003@gmail.com>
2022-06-24 18:46:06 +0530

    Create styles for seract bar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import "./Header.scss";
 import UserImage from "../../images/user-image.jpeg";
 
 const Header = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const searchMoviesAndShowHandler = (e) => {
+    e.preventDefault();
+    console.log(searchQuery);
+  };
+
   return (
     <div className="header">
       <Link to="/">
         <div className="logo">Movie App</div>
       </Link>
+      <div className="search-bar">
+        <form onSubmit={searchMoviesAndShowHandler}>
+          <input
+            type="text"
+            placeholder="Search movies or shows 👀"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
+          <button type="sumit">
+            <i class="fa-solid fa-magnifying-glass"></i>
+          </button>
+        </form>
+      </div>
       <div className="user-image">
         <img src={UserImage} alt="user" />
       </div>
